869974fc9d79cb670f2da960bd558f0bc5458648
Mark Deutsch <mark.deutsch@duke.edu>
2019-07-01 14:34:05 -0400

    Add eslintrc

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import PropTypes from 'prop-types';
 export default class App extends React.Component {
   static propTypes = {
     actAs: PropTypes.string.isRequired,
-    match: PropTypes.object.isRequired,
-    stripes: PropTypes.object.isRequired,
   }
 
   render() {
-    const { actAs, match: { path } } = this.props;
+    const { actAs } = this.props;
 
     return (
       <div>
         <h2>Local KB Admin</h2>
-        <h4>Acting as: {actAs}</h4>
+        <h4>
+          Acting as:
+          {actAs}
+        </h4>
       </div>
     );
   }
