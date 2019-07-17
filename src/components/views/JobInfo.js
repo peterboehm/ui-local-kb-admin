056c64422291212433ba7bba44ddd96cd3de9cad
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-07-17 11:49:33 +0100

    add job view route

--- src/components/views/JobInfo.js
+++ src/components/views/JobInfo.js
@@ -1,52 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage, FormattedDate } from 'react-intl';
 import { get } from 'lodash';
-import { FormattedDate, FormattedMessage } from 'react-intl';
-import { Col, KeyValue, Row } from '@folio/stripes/components';
+import { NotesSmartAccordion } from '@folio/stripes/smart-components';
 
+import {
+  AccordionSet,
+  Button,
+  Col,
+  ExpandAllButton,
+  Icon,
+  Layout,
+  Pane,
+  PaneMenu,
+  KeyValue,
+  Row,
+} from '@folio/stripes/components';
+import { AppIcon, IfPermission, TitleManager } from '@folio/stripes/core';
+import { Spinner } from '@folio/stripes-erm-components';
 
-export default class JobInfo extends React.Component {
+class JobInfo extends React.Component {
   static propTypes = {
-    className: PropTypes.string,
-    job: PropTypes.shape({
-      id: PropTypes.string,
-      dateCreated: PropTypes.string,
-      logEntries: PropTypes.array,
-      name: PropTypes.string,
-      ended: PropTypes.string,
-      started: PropTypes.string,
-      status: PropTypes.shape({
-        id: PropTypes.string,
-        value: PropTypes.string,
-        label: PropTypes.string,
-      }),
-      result: PropTypes.shape({
-        id: PropTypes.string,
-        value: PropTypes.string,
-        label: PropTypes.string,
-      }),
+    data: PropTypes.shape({
+      job: PropTypes.object,
     }),
-    renderName: PropTypes.bool,
+    handlers: PropTypes.shape({
+      onClose: PropTypes.func.isRequired,
+    }).isRequired,
+    isLoading: PropTypes.bool,
+    stripes: PropTypes.object,
+  };
+
+  renderLoadingPane = () => {
+    return (
+      <Pane
+        defaultWidth="45%"
+        dismissible
+        id="pane-view-job"
+        onClose={this.props.onClose}
+        paneTitle={<FormattedMessage id="ui-local-kb-admin.loading" />}
+      >
+        <Layout className="marginTop1">
+          <Spinner />
+        </Layout>
+      </Pane>
+    );
   }
 
-  static defaultProps = {
-    job: {},
-    renderName: true,
-  }
-
-  /* renderLicensor = () => {
-    const { license: { orgs = [] } } = this.props;
-    const licensor = orgs.find(o => get(o, ['role', 'value']) === 'licensor');
-    const licensorName = get(licensor, ['org', 'name']) || <FormattedMessage id="stripes-erm-components.licenseCard.notSet" />;
-
-    return licensorName;
-  } */
-
-  renderName = () => {
-    const { job, renderName } = this.props;
-
-    if (!renderName) return null;
-
+  renderName = (job) => {
     return (
       <Row>
         <Col xs={12}>
@@ -59,58 +60,80 @@ export default class JobInfo extends React.Component {
   }
 
   render() {
-    const { job, className } = this.props;
+    const { data: { job }, isLoading, handlers } = this.props;
+
+    if (isLoading) return this.renderLoadingPane();
 
     return (
-      <div className={className}>
-        { this.renderName() }
-        <Row>
-          <Col xs={4}>
-            <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.runningStatus" />}>
-              <div data-test-job-status>
-                {get(job, ['status', 'label'], '-')}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={4}>
-            <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.result" />}>
-              <div data-test-job-result>
-                {get(job, ['result', 'label'], '-')}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={4}>
-            <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.errors" />}>
-              <div data-test-job-errors>
-                {'errors'}
-              </div>
-            </KeyValue>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={4}>
-            <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.started" />}>
-              <div data-test-job-started>
-                {job.started ? <FormattedDate value={job.started} /> : '-'}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={4}>
-            <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.ended" />}>
-              <div data-test-job-ended>
-                {job.ended ? <FormattedDate value={job.ended} /> : '-'}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={4}>
-            <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.filename" />}>
-              <div data-test-job-filename>
-                {'filename'}
-              </div>
-            </KeyValue>
-          </Col>
-        </Row>
-      </div>
+      <Pane
+        defaultWidth="45%"
+        dismissible
+        id="pane-view-job"
+        onClose={this.props.onClose}
+        paneTitle={job.name}
+      >
+        <TitleManager record={job.name}>
+          <div>
+            <Row>
+              <Col xs={12}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.jobName" />}>
+                  <div data-test-job-name>
+                    { this.renderName(job) }
+                  </div>
+                </KeyValue>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={4}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.runningStatus" />}>
+                  <div data-test-job-status>
+                    {get(job, ['status', 'label'], '-')}
+                  </div>
+                </KeyValue>
+              </Col>
+              <Col xs={4}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.result" />}>
+                  <div data-test-job-result>
+                    {get(job, ['result', 'label'], '-')}
+                  </div>
+                </KeyValue>
+              </Col>
+              <Col xs={4}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.errors" />}>
+                  <div data-test-job-errors>
+                    {'errors'}
+                  </div>
+                </KeyValue>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={4}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.started" />}>
+                  <div data-test-job-started>
+                    {job.started ? <FormattedDate value={job.started} /> : '-'}
+                  </div>
+                </KeyValue>
+              </Col>
+              <Col xs={4}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.ended" />}>
+                  <div data-test-job-ended>
+                    {job.ended ? <FormattedDate value={job.ended} /> : '-'}
+                  </div>
+                </KeyValue>
+              </Col>
+              <Col xs={4}>
+                <KeyValue label={<FormattedMessage id="ui-local-kb-admin.prop.filename" />}>
+                  <div data-test-job-filename>
+                    {'filename'}
+                  </div>
+                </KeyValue>
+              </Col>
+            </Row>
+          </div>
+        </TitleManager>
+      </Pane>
     );
   }
 }
+
+export default JobInfo;
--- src/routes/ViewJobRoute.js
+++ src/routes/ViewJobRoute.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 
 import { stripesConnect } from '@folio/stripes/core';
 
+import View from '../components/views';
+
 
 class ViewJobRoute extends React.Component {
+  static manifest = Object.freeze({
+    job: {
+      type: 'okapi',
+      path: 'erm/jobs/:{id}',
+    },
+  });
+
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+      search: PropTypes.string.isRequired,
+    }).isRequired,
+    resources: PropTypes.shape({
+      interfaces: PropTypes.object,
+      linkedAgreements: PropTypes.object,
+      license: PropTypes.object,
+      terms: PropTypes.object,
+      users: PropTypes.object,
+    }).isRequired,
+    stripes: PropTypes.shape({
+      hasPerm: PropTypes.func.isRequired,
+      okapi: PropTypes.object.isRequired,
+    }).isRequired,
+  };
+
+  handleClose = () => {
+    this.props.history.push(`/local-kb-admin${this.props.location.search}`);
+  }
 
   render() {
-    console.log('here');
+    const { resources } = this.props;
     return (
-      <div>
-        view page
-      </div>
+      <View
+        data={{
+          job: {
+            ...get(resources, 'job.records[0]', {}),
+          },
+        }}
+        onClose={this.handleClose}
+        isLoading={get(resources, 'job.isPending', true)}
+      />
     );
   }
 }
