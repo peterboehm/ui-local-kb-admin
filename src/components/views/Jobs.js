a74535e903164c839603a7fb19109543b7ef2bb6
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-07-19 13:30:09 +0100

    revert initialSortState

--- src/components/views/Jobs.js
+++ src/components/views/Jobs.js
@@ -173,7 +173,7 @@ export default class Jobs extends React.Component {
       <div data-test-localkbadmin ref={contentRef}>
         <SearchAndSortQuery
           initialFilterState={{ status: ['Queued', 'In progress'] }}
-          initialSortState={{ sort: ['status', 'started'] }}
+          initialSortState={{ sort: 'started' }}
           initialSearchState={{ query: '' }}
           queryGetter={queryGetter}
           querySetter={querySetter}
