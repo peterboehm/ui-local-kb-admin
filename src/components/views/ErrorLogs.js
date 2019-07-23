e38d9f0aa95ad1a7013d181ff87c7b517badcd44
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-07-23 13:44:40 +0200

    modify log accordions

--- src/components/views/ErrorLogs.js
+++ src/components/views/ErrorLogs.js
@@ -1,30 +1,60 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
-import { Accordion } from '@folio/stripes/components';
+import { get } from 'lodash';
+import { Accordion, Badge, MultiColumnList } from '@folio/stripes/components';
 
 export default class ErrorLogs extends React.Component {
   static propTypes = {
     id: PropTypes.string,
+    job: PropTypes.object,
     onToggle: PropTypes.func,
     open: PropTypes.bool,
   };
 
-  renderErrorLogs = () => {
-    return <FormattedMessage id="ui-local-kb-admin.errorLogNo" />;
+  renderBadge = () => {
+    const count = this.job.errorLog.length;
+    return count !== undefined ? <Badge>{count}</Badge> : <Badge>0</Badge>;
+  }
+
+  renderErrorLogs = (id, job) => {
+    const { errorLog } = job;
+    if (errorLog) {
+      const { recordNumber } = errorLog;
+      return (
+        <MultiColumnList
+          contentData={errorLog}
+          id={`list-${id}`}
+          visibleColumns={['recordNumber', 'message']}
+          columnMapping={{
+            recordNumber: <FormattedMessage id="ui-local-kb-admin.columns.recordNumber" />,
+            message: <FormattedMessage id={`ui-local-kb-admin.columns.${id}Message`} />,
+          }}
+          formatter={{
+            recordNumber: () => {
+              if (!recordNumber) return '-';
+              return recordNumber;
+            },
+          }}
+        />
+      );
+    } else {
+      return <FormattedMessage id={`ui-local-kb-admin.${id}No`} />;
+    }
   }
 
   render() {
-    const { id, onToggle, open } = this.props;
-
+    const { id, job, onToggle, open } = this.props;
     return (
       <Accordion
+        displayWhenClosed={this.renderBadge()}
+        displayWhenOpen={this.renderBadge()}
         id={id}
-        label={<FormattedMessage id="ui-local-kb-admin.errorLog" />}
+        label={<FormattedMessage id={`ui-local-kb-admin.${id}`} />}
         onToggle={onToggle}
         open={open}
       >
-        { this.renderErrorLogs() }
+        { this.renderErrorLogs(id, job) }
       </Accordion>
     );
   }
--- src/components/views/InfoLogs.js
+++ src/components/views/InfoLogs.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
-import { Accordion, MultiColumnList } from '@folio/stripes/components';
+import { get } from 'lodash';
+import { Accordion, Badge, MultiColumnList } from '@folio/stripes/components';
 
 export default class InfoLogs extends React.Component {
   static propTypes = {
@@ -11,17 +12,34 @@ export default class InfoLogs extends React.Component {
     open: PropTypes.bool,
   };
 
+  renderBadge = () => {
+    const count = get(this.props.job, ['infoLog', 'length']);
+    return count !== undefined ? <Badge>{count}</Badge> : <Badge>0</Badge>;
+  }
+
   renderInfoLogs = (id, job) => {
     const { infoLog } = job;
     if (infoLog) {
+      const { recordNumber } = infoLog;
       return (
         <MultiColumnList
           contentData={infoLog}
           id={`list-${id}`}
+          visibleColumns={['recordNumber', 'message']}
+          columnMapping={{
+            recordNumber: <FormattedMessage id="ui-local-kb-admin.columns.recordNumber" />,
+            message: <FormattedMessage id={`ui-local-kb-admin.columns.${id}Message`} />,
+          }}
+          formatter={{
+            recordNumber: () => {
+              if (!recordNumber) return '-';
+              return recordNumber;
+            },
+          }}
         />
       );
     } else {
-      return <FormattedMessage id="ui-local-kb-admin.infoLogNo" />;
+      return <FormattedMessage id={`ui-local-kb-admin.${id}No`} />;
     }
   }
 
@@ -29,8 +47,10 @@ export default class InfoLogs extends React.Component {
     const { id, job, onToggle, open } = this.props;
     return (
       <Accordion
+        displayWhenClosed={this.renderBadge()}
+        displayWhenOpen={this.renderBadge()}
         id={id}
-        label={<FormattedMessage id="ui-local-kb-admin.infoLog" />}
+        label={<FormattedMessage id={`ui-local-kb-admin.${id}`} />}
         onToggle={onToggle}
         open={open}
       >
