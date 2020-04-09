627fb6a98d172c4790d2d9f4ecabea5528bb31d4
Mark Deutsch <mark.deutsch@duke.edu>
2020-04-09 16:35:48 -0400

    remove describe.only

--- test/bigtest/tests/settings-externalDataSources-test.js
+++ test/bigtest/tests/settings-externalDataSources-test.js
@@ -5,7 +5,7 @@ import { expect } from 'chai';
 import setupApplication from '../helpers/setup-application';
 import ExternalDataSourcesInteractor from '../interactors/external-data-sources';
 
-describe.only('External Data Source Settings', () => {
+describe('External Data Source Settings', () => {
   setupApplication();
   const externaldatasources = new ExternalDataSourcesInteractor();
 
